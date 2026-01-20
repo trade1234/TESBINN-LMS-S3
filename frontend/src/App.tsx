3bad71363c5681e5bb214a4cf33a90ae1b95653d
Amannel16 <andemoamanuel@gmail.com>
2026-01-20 11:54:22 +0300

    feat(ScrollToTop): add ScrollToTop component for smooth scrolling on route changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/layout/ScrollToTop";
 
 // Pages
 import Landing from "./pages/Landing";
@@ -52,6 +53,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           {/* Public Routes */}
           <Route path="/" element={<Landing />} />
